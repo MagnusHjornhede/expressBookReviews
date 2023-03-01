96327b905649c1cc2c2af7938dfc83972e787831
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2023-03-01 15:47:08 +0100

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,5 +1,5 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": "Just made a review for testing" },
       2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
       3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
       4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
@@ -12,3 +12,4 @@ let books = {
 }
 
 module.exports=books;
+
