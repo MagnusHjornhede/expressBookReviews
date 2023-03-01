8541b7ec16c315b3b8733aec413c62386a00e25c
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2023-03-01 12:53:52 +0100

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,69 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => {
+  //Check is the username is valid
+  let userswithsamename = users.filter((user) => {
+    return user.username = username;
+  })
+  if (userswithsamename.length > 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+  //Check if username and password match the one we have in records.
+  let validusers = users.filter((user) => {
+    return (user.username === username && user.password === password)
+  });
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  
+  const { username, password } = req.body;
+  if (!authenticatedUser(username, password)) {
+    // Return 401 Unauthorized status if the user is not authenticated
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const user = { username: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  // Return the JWT token as a JSON response
+  res.json({ accessToken: accessToken });
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;  // req.params.isbn ?
+  const { rating, comment } = req.body;
+  
+  // Find the book with the given ISBN
+  const book = books.find((book) => book.isbn === isbn);
+  
+  // Return a 404 Not Found status if the book is not found
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  
+  // Create a new review object
+  const review = { rating: rating, comment: comment };
+  
+  // Add the review to the book's reviews array
+  book.reviews.push(review);
+  
+  // Return the updated book object as a JSON response
+  res.json(book);
 });
 
 module.exports.authenticated = regd_users;
