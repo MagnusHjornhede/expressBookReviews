15815ef397bd14b484cc9a1fd416958ac3e8f06c
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2023-03-08 23:19:35 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,10 +12,9 @@ app.use("/customer", session({ secret: "fingerprint_customer", resave: true, sav
 
 app.use("/customer/auth/*", function auth(req, res, next) {
     // Middleware which tells that the user is authenticated or not
-    /*
     if (req.session.authorization) {
         let token = req.session.authorization['accessToken']; // Access Token
-        jwt.verify(token, "access", (err, user) => {
+        jwt.verify(token, "accessToken", (err, user) => {
             if (!err) {
                 req.user = user;
                 next();
@@ -25,30 +24,8 @@ app.use("/customer/auth/*", function auth(req, res, next) {
             }
         });
     } else {
-        return res.status(403).json({ message: "User not logged in" })
-    }*/
-
-    
-     const token = req.headers.authorization?.split(" ")[1];
-     // Check for token
-     if (!token) {
-      // return res.status(401).json({ message: "Authentication failed. Token not provided." });
-     }
-     
-     try {
-       const decoded = jwt.verify(token, "secret_key");
-       const userId = decoded.userId;
-       const user = users.find((user) => user.id === userId);
-   
-       if (!user) {
-         return res.status(401).json({ message: "Authentication failed. User not found." });
-       }
-   
-       req.user = user;
-       next();
-     } catch (error) {
-       return res.status(401).json({ message: "Authentication failed. Invalid token." });
-     }
+        return res.status(403).json({ message: "INDEX- User not logged in" })
+    }
 });
 
 const PORT = 5000;
