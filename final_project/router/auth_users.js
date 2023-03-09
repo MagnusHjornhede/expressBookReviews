75b0252237243998de6031253a3816e1f05eec49
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2023-03-09 14:42:37 +0100

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
+//let books = require("./booksdb.js");
 const regd_users = express.Router();
+
+let books = {
+    1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": "Just made a review for testing" },
+    2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+    3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+    4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+    5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
+    6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+    7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+    8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+    9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+    10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+}
 
 let users = [{ "username": "22", "password": "22" }];
 
@@ -61,7 +74,7 @@ regd_users.post("/login", (req, res) => {
     // res.json({ accessToken });
     return res.status(200).send("User successfully logged in");
 });
-
+/*
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
     const { isbn } = req.params;
@@ -79,14 +92,45 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     }
 
     // Create a new review object
-    const review = { comment: comment };
-
+   // const review = { comment: comment };
+    const review = {
+        "username": "user3",
+        "text": "This book was okay.",
+        "date": "2022-03-12"
+      };
     // Add the review to the book's reviews array
     book.reviews.push(review);
 
     // Return the updated book object as a JSON response
     res.json(book);
 });
+*/
+
+regd_users.put("/auth/review/:isbn", (req, res) => {
+    const { isbn } = req.params;
+    const { username, comment } = req.body;
+
+    const book = books[isbn];
+
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    const review = {
+        username,
+        text: comment,
+        date: new Date().toISOString()
+    };
+
+   // if (!book.reviews) {
+      //  book.reviews = [review];
+   // } else {
+        books.push(book);
+  //  }
+
+    return res.json(book);
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
