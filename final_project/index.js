4b8efe3d8774a8e953bdf5f82e66283296f262d6
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2023-03-16 22:04:37 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -28,55 +28,6 @@ if(req.session.authorization) {
  }
 });
 
-/*
-app.use("/customer/auth/*", function auth(req, res, next) {
-    // Check if the user is logged in
-    if (req.session && req.session.user) {
-      // Get the user object from the session
-      const user = req.session.user;
-  
-      // Check if the user has a valid access token
-      if (user.accessToken) {
-        // Verify the access token using the JWT library
-        jwt.verify(user.accessToken, 'accessToken', (err, decoded) => {
-          if (err) {
-            // Access token is invalid or expired, send 403 Forbidden status
-            return res.status(403).json({ message: "User not authenticated" });
-          } else {
-            // Access token is valid, store the user object in the request
-            req.user = decoded.data;
-            next();
-          }
-        });
-      } else {
-        // User does not have a valid access token, send 403 Forbidden status
-        return res.status(403).json({ message: "User not authenticated" });
-      }
-    } else {
-      // User is not logged in, send 403 Forbidden status
-      return res.status(403).json({ message: "User not logged in" });
-    }
-  });
-*/
-/*
-app.use("/customer/auth/*", function auth(req, res, next) {
-    // Middleware which tells that the user is authenticated or not
-    if (req.session.authorization) {
-        let token = req.session.authorization['accessToken']; // Access Token
-        jwt.verify(token, "accessToken", (err, user) => {
-            if (!err) {
-                req.user = user;
-                next();
-            }
-            else {
-                return res.status(403).json({ message: "User not authenticated" })
-            }
-        });
-    } else {
-        return res.status(403).json({ message: "INDEX- User not logged in" })
-    }
-});
-*/
 const PORT = 5000;
 
 app.use("/customer", customer_routes);
